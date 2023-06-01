eb1035ba9db44ab855555dacbd225818eee1e4ca
Matthew Bateson <batesonmatt@yahoo.com>
2023-06-01 13:00:40 -0500

    Added MiscTests class with GetTotalLineCount test method.

---
+++ DST.Core.Tests/MiscTests.cs
@@ -0,0 +1,34 @@
+﻿namespace DST.Core.Tests
+{
+    internal static class MiscTests
+    {
+        // Usage:
+        // GetLines("C:\\Users\\bates\\source\\repos\\Messier\\DST.Core\\", "*.cs", SearchOption.AllDirectories)
+        public static int GetTotalLineCount(string searchDirectory, string searchPattern, SearchOption option)
+        {
+            if (string.IsNullOrWhiteSpace(searchDirectory)) return -1;
+            if (string.IsNullOrWhiteSpace(searchDirectory)) return -2;
+            if (Directory.Exists(searchDirectory) == false) return -3;
+
+            int lines = 0;
+            string[] files;
+
+            try
+            {
+                files = Directory.GetFiles(searchDirectory, searchPattern, option);
+
+                foreach (string file in files)
+                {
+                    lines += File.ReadLines(file).Count();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                lines = -4;
+            }
+
+            return lines;
+        }
+    }
+}
