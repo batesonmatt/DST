49025cc0836ad0ac97c5323a1a837a1c358d5da7
Matthew Bateson <batesonmatt@yahoo.com>
2024-02-10 20:56:54 -0600

    Added VectorFactory.CreateLocal and VectorFactory.CreateNonTrackable, and revised call site in VectorFactory.Create. Fixed references to IVector.DateTime.

--- DST.Core/Vector/VectorFactory.cs
+++ DST.Core/Vector/VectorFactory.cs
@@ -5,17 +5,30 @@ namespace DST.Core.Vector
 {
     public class VectorFactory
     {
-        // Returns a new IVector object given the specified IBaseDateTime and ICoordinate arguments.
-        public static IVector Create(IBaseDateTime dateTime, ICoordinate coordinate)
+        // Returns a new IVector object given the specified IMutableDateTime and ICoordinate arguments.
+        public static IVector Create(IMutableDateTime dateTime, ICoordinate coordinate)
         {
             _ = dateTime ?? throw new ArgumentNullException(nameof(dateTime));
             _ = coordinate ?? throw new ArgumentNullException(nameof(coordinate));
 
             return coordinate switch
             {
-                IHorizontalCoordinate position => new LocalVector(dateTime, position),
+                IHorizontalCoordinate position => CreateLocal(dateTime, position),
+                NonTrackableCoordinate nonTrackable => CreateNonTrackable(dateTime, nonTrackable),
                 _ => throw new NotSupportedException($"{nameof(ICoordinate)} type '{coordinate.GetType()}' is not supported.")
             };
+        }
+
+        // Returns a new ILocalVector object given the specified IMutableDateTime and IHorizontalCoordinate arguments.
+        private static ILocalVector CreateLocal(IMutableDateTime dateTime, IHorizontalCoordinate coordinate)
+        {
+            return new LocalVector(dateTime, coordinate);
+        }
+
+        // Returns a new ILocalVector object given the specified IMutableDateTime and NonTrackableCoordinate arguments.
+        private static NonTrackableVector CreateNonTrackable(IMutableDateTime dateTime, NonTrackableCoordinate coordinate)
+        {
+            return new NonTrackableVector(dateTime, coordinate);
         }
     }
 }
