b32991aa63e127fc46787aa51508098bc4d38598
Matthew Bateson <batesonmatt@yahoo.com>
2024-02-10 20:54:52 -0600

    Added a NonTrackableVector implementation for IVector to represent a non-observable position as a result of failed tracking due to an out of range datetime.

---
+++ DST.Core/Vector/NonTrackableVector.cs
@@ -0,0 +1,23 @@
+﻿using DST.Core.Coordinate;
+using DST.Core.DateAndTime;
+
+namespace DST.Core.Vector
+{
+    // Represents a non-observable position of a celestial object that was a result of failed tracking.
+    // This class is not intended to be instantiated in public assemblies.
+    public class NonTrackableVector : IVector
+    {
+        // Gets this NonTrackableVector's date/time value.
+        public IMutableDateTime DateTime { get; }
+
+        // Gets this NonTrackableVector's ICoordinate object.
+        public ICoordinate Coordinate { get; }
+
+        // Creates a new NonTrackableVector instance with the specified IMutableDateTime and NonTrackableCoordinate arguments.
+        internal NonTrackableVector(IMutableDateTime dateTime, NonTrackableCoordinate coordinate)
+        {
+            DateTime = dateTime ?? throw new ArgumentNullException(nameof(dateTime));
+            Coordinate = coordinate ?? throw new ArgumentNullException(nameof(coordinate));
+        }
+    }
+}
