cf99100b49d95808f85a5858a783fdfee7985ef3
Matthew Bateson <batesonmatt@yahoo.com>
2024-02-10 20:38:23 -0600

    Added ComponentsFactory.Create overload to return a new IComponents object with default Angle values.

--- DST.Core/Components/ComponentsFactory.cs
+++ DST.Core/Components/ComponentsFactory.cs
@@ -4,6 +4,12 @@ namespace DST.Core.Components
 {
     public class ComponentsFactory
     {
+        // Returns a new IComponents object with default Angle values.
+        public static IComponents Create()
+        {
+            return new SphericalComponents(Angle.Zero, Angle.Zero);
+        }
+
         // Returns a new IComponents object given the specified angular rotation and inclination Angle values.
         public static IComponents Create(Angle rotation, Angle inclination)
         {
