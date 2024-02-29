f3ac83003d8e10b88025d6c4129830339282ec16
Matthew Bateson <batesonmatt@yahoo.com>
2024-02-29 16:08:35 -0600

    Changed TrackPhaseViewModel.Phases property to be of type List<SelectListItem>.

--- DST/Models/ViewModels/TrackPhaseViewModel.cs
+++ DST/Models/ViewModels/TrackPhaseViewModel.cs
@@ -2,6 +2,7 @@
 using DST.Models.BusinessLogic;
 using DST.Models.DomainModels;
 using DST.Models.Routes;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 
 namespace DST.Models.ViewModels
@@ -13,7 +14,7 @@ namespace DST.Models.ViewModels
         public DsoModel Dso { get; set; }
         public TrackPhaseRoute CurrentRoute { get; set; }
         public IEnumerable<TrackAlgorithmItem> Algorithms { get; set; }
-        public IEnumerable<TrackPhaseItem> Phases { get; set; }
+        public List<SelectListItem> Phases { get; set; }
         public TrackPhaseModel TrackForm { get; set; }
         public IEnumerable<IVector> Results { get; set; }
 
