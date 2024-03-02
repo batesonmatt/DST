3c77d28ee5b9b5c829d607a899541a8c9b5965d1
Matthew Bateson <batesonmatt@yahoo.com>
2024-03-01 23:44:43 -0600

    Added warning message string and conditional messages for determining state.

--- DST/Models/ViewModels/TrackPhaseViewModel.cs
+++ DST/Models/ViewModels/TrackPhaseViewModel.cs
@@ -4,6 +4,7 @@ using DST.Models.DomainModels;
 using DST.Models.Routes;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DST.Models.ViewModels
 {
@@ -17,6 +18,26 @@ namespace DST.Models.ViewModels
         public List<SelectListItem> Phases { get; set; }
         public TrackPhaseModel TrackForm { get; set; }
         public IEnumerable<IVector> Results { get; set; }
+        public string WarningMessage { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public bool HasResults()
+        {
+            return Results?.Any() ?? false;
+        }
+
+        public bool IsTrackingSupported()
+        {
+            return Phases?.Any() ?? false;
+        }
+
+        public bool HasWarning()
+        {
+            return !string.IsNullOrWhiteSpace(WarningMessage);
+        }
 
         #endregion
     }
