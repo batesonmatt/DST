01c8818a8c462d3e8ad2da571700754d9839ef62
Matthew Bateson <batesonmatt@yahoo.com>
2024-03-06 22:36:07 -0600

    Added Trajectory.Phase enum values to differentiate between the various phases.

---
+++ DST.Core/Trajectory/Phase.cs
@@ -0,0 +1,11 @@
+﻿namespace DST.Core.Trajectory
+{
+    // Provides values for various types of trajectory phases.
+    public enum Phase
+    {
+        Rise,
+        Apex,
+        Set,
+        Default = Rise
+    }
+}
