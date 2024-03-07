d110d2b38c68cbcd871d5d216066c5c7943a67e7
Matthew Bateson <batesonmatt@yahoo.com>
2024-03-06 22:34:15 -0600

    Added TrackPhaseResult to help format ILocalVector properties to be displayed as text.

---
+++ DST/Models/BusinessLogic/TrackPhaseResult.cs
@@ -0,0 +1,51 @@
+﻿using DST.Core.Vector;
+
+namespace DST.Models.BusinessLogic
+{
+    public class TrackPhaseResult
+    {
+        #region Properties
+
+        public ILocalVector Vector { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public TrackPhaseResult(ILocalVector vector)
+        {
+            Vector = vector;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public string GetUniversalDateTimeText()
+        {
+            return Vector.DateTime.ToString();
+        }
+
+        public string GetLocalDateTimeText()
+        {
+            return Vector.DateTime.ToLocalTime().ToString();
+        }
+
+        public string GetAltitudeText()
+        {
+            return Vector.Position.Format(Core.Coordinate.FormatType.Component, Core.Components.ComponentType.Inclination);
+        }
+
+        public string GetAzimuthText()
+        {
+            return Vector.Position.Format(Core.Coordinate.FormatType.Component, Core.Components.ComponentType.Rotation);
+        }
+
+        public string GetCoordinateText()
+        {
+            return Vector.Coordinate.Format(Core.Coordinate.FormatType.Component);
+        }
+
+        #endregion
+    }
+}
