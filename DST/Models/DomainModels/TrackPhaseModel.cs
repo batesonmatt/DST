15d23acfc4bcc6ba69ea13e88ee2d26221e06bda
Matthew Bateson <batesonmatt@yahoo.com>
2024-04-11 00:17:03 -0500

    Revised TrackPhaseModel to inherit from TrackModel.

--- DST/Models/DomainModels/TrackPhaseModel.cs
+++ DST/Models/DomainModels/TrackPhaseModel.cs
@@ -1,43 +1,18 @@
 ﻿using DST.Models.DataLayer.Query;
-using DST.Models.Validation;
-using Microsoft.AspNetCore.Mvc;
 using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DST.Models.DomainModels
 {
-    public class TrackPhaseModel
+    public class TrackPhaseModel : TrackModel
     {
         #region Properties
 
-        public string Algorithm { get; set; } = AlgorithmName.Default;
-
         public string Phase { get; set; } = PhaseName.Default;
-
-        [Required(ErrorMessage = "Please enter the start date.")]
-        [Remote("ValidateStartDate", "Validation")]
-        [EpochDateRange]
-        public DateTime Start { get; set; }
 
         [Required(ErrorMessage = "Please enter the number of cycles.")]
         [Range(-100, 100, ErrorMessage = "The number of cycles must be between -100 and 100.")]
         public int Cycles { get; set; } = 0;
-
-        public bool IsReady { get; set; } = false;
-
-        #endregion
-
-        #region Methods
-
-        public long GetTicks()
-        {
-            return Start.Ticks;
-        }
-
-        public string GetFormattedStart()
-        {
-            return Start.ToString("yyyy-MM-ddTHH:mm:ss");
-        }
 
         #endregion
     }
