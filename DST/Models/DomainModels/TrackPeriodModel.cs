b1595c1b7bba1ea5c14901a506a3ba7bd86066f7
Matthew Bateson <batesonmatt@yahoo.com>
2024-04-11 00:16:38 -0500

    Added TrackPeriodModel class for submitting period tracking forms.

---
+++ DST/Models/DomainModels/TrackPeriodModel.cs
@@ -0,0 +1,25 @@
+﻿using DST.Models.DataLayer.Query;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace DST.Models.DomainModels
+{
+    public class TrackPeriodModel : TrackModel
+    {
+        #region Properties
+
+        public bool IsFixed { get; set; } = false;
+
+        public string TimeUnit { get; set; } = TimeUnitName.Default;
+
+        [Required(ErrorMessage = "Please enter the period length.")]
+        [Remote("ValidatePeriod", "Validation", AdditionalFields = "Algorithm, TimeUnit, IsFixed")]
+        public int Period { get; set; }
+
+        [Required(ErrorMessage = "Please enter the number of intervals.")]
+        [Remote("ValidateInterval", "Validation", AdditionalFields = "Period")]
+        public int Interval { get; set; }
+
+        #endregion
+    }
+}
