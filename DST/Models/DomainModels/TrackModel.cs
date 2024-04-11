874c08d42ba453701ec3bb1e32679e5bb49b0a1d
Matthew Bateson <batesonmatt@yahoo.com>
2024-04-11 00:14:49 -0500

    Added TrackModel base class.

---
+++ DST/Models/DomainModels/TrackModel.cs
@@ -0,0 +1,38 @@
+﻿using DST.Models.DataLayer.Query;
+using DST.Models.Validation;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DST.Models.DomainModels
+{
+    public class TrackModel
+    {
+        #region Properties
+
+        public string Algorithm { get; set; } = AlgorithmName.Default;
+
+        [Required(ErrorMessage = "Please enter the start date.")]
+        [Remote("ValidateStartDate", "Validation")]
+        [EpochDateRange]
+        public DateTime Start { get; set; }
+
+        public bool IsReady { get; set; } = false;
+
+        #endregion
+
+        #region Methods
+
+        public long GetTicks()
+        {
+            return Start.Ticks;
+        }
+
+        public string GetFormattedStart()
+        {
+            return Start.ToString("yyyy-MM-ddTHH:mm:ss");
+        }
+
+        #endregion
+    }
+}
