88acbb0cdbe48b8773737164ddc79ff10e1ac10c
Matthew Bateson <batesonmatt@yahoo.com>
2024-04-18 23:41:00 -0500

    Updated comments. Renamed boolean parameter to reflect the concept of aggregated intervals.

--- DST.Core/DateTimesBuilder/DateTimesBuilderFactory.cs
+++ DST.Core/DateTimesBuilder/DateTimesBuilderFactory.cs
@@ -5,12 +5,12 @@ namespace DST.Core.DateTimesBuilder
     public class DateTimesBuilderFactory
     {
         // Creates a new IDateTimesBuilder object given the specified IDateTimeAdder object and whether
-        // to add each interval from the starting or previous date/time value.
-        public static IDateTimesBuilder Create(IDateTimeAdder dateTimeAdder, bool addFromStart)
+        // to add each interval from the starting date (aggregated) or previous date (successive).
+        public static IDateTimesBuilder Create(IDateTimeAdder dateTimeAdder, bool aggregate)
         {
             _ = dateTimeAdder ?? throw new ArgumentNullException(nameof(dateTimeAdder));
 
-            return addFromStart switch
+            return aggregate switch
             {
                 true => new LongDateTimesBuilder(dateTimeAdder),
                 _ => new ShortDateTimesBuilder(dateTimeAdder)
