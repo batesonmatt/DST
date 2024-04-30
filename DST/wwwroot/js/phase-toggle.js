316cebee76dd94c04f9c724af2275eb7fae17487
Matthew Bateson <batesonmatt@yahoo.com>
2024-04-29 23:32:07 -0500

    Added phaseTrackOnce JavaScript function for disabling/enabling and zeroing the Phase Cycles form field.

---
+++ DST/wwwroot/js/phase-toggle.js
@@ -0,0 +1,18 @@
+﻿function phaseTrackOnce()
+{
+    let t = document.getElementById("input-phase-single");
+    let e = document.getElementById("input-phase-cycles");
+
+    if (t.checked === true)
+    {
+        e.value = 0;
+        e.setAttribute("disabled", "disabled");
+    }
+    else
+    {
+        if (e.hasAttribute("disabled"))
+        {
+            e.removeAttribute("disabled");
+        }
+    }
+}
