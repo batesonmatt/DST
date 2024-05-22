c41b02a7fb567dac6191e0f87d4aff7d5a2d99fe
Matthew Bateson <batesonmatt@yahoo.com>
2024-05-21 23:48:11 -0500

    Added DsoDetailsViewModel class.

---
+++ DST/Models/ViewModels/DsoDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DST.Models.BusinessLogic;
+using DST.Models.DomainModels;
+
+namespace DST.Models.ViewModels
+{
+    public class DsoDetailsViewModel
+    {
+        #region Properties
+
+        public DsoModel Dso { get; set; }
+        public DsoDetailsInfo DetailsInfo { get; set; }
+
+        #endregion
+    }
+}
