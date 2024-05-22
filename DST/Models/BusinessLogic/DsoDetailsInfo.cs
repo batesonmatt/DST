199caec66c92488ede97d4ae4c17491ca0dc19d6
Matthew Bateson <batesonmatt@yahoo.com>
2024-05-21 23:47:42 -0500

    Added DsoDetailsInfo dictionary.

---
+++ DST/Models/BusinessLogic/DsoDetailsInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DST.Models.BusinessLogic
+{
+    public class DsoDetailsInfo : Dictionary<DsoDetailsItem, Tuple<string, string>> { }
+}
