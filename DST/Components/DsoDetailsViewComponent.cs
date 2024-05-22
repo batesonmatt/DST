68b51f7d668919d49907b20858c42ff30bb09bb0
Matthew Bateson <batesonmatt@yahoo.com>
2024-05-21 23:49:55 -0500

    Added DsoDetailsViewComponent for the Dso details partial view.

---
+++ DST/Components/DsoDetailsViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using DST.Models.BusinessLogic;
+using DST.Models.DomainModels;
+using DST.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DST.Components
+{
+    public class DsoDetailsViewComponent : ViewComponent
+    {
+        #region Constructors
+
+        public DsoDetailsViewComponent() { }
+
+        #endregion
+
+        #region Methods
+
+        public IViewComponentResult Invoke(DsoModel dso)
+        {
+            DsoDetailsViewModel viewModel = new()
+            {
+                Dso = dso,
+                DetailsInfo = Utilities.GetDetailsInfo(dso)
+            };
+
+            return View("~/Views/Shared/_DsoDetailsPartial.cshtml", viewModel);
+        }
+
+        #endregion
+    }
+}
