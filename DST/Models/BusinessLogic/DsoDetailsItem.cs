b5b83fca87a3be97b2538bc0c261befca4dc6401
Matthew Bateson <batesonmatt@yahoo.com>
2024-05-21 23:47:16 -0500

    Added DsoDetailsItem enum.

---
+++ DST/Models/BusinessLogic/DsoDetailsItem.cs
@@ -0,0 +1,15 @@
+﻿namespace DST.Models.BusinessLogic
+{
+    public enum DsoDetailsItem
+    {
+        OtherNames,
+        Catalog,
+        Type,
+        Description,
+        Constellation,
+        RightAscension,
+        Declination,
+        Distance,
+        Magnitude
+    }
+}
