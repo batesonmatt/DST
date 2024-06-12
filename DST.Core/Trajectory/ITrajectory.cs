d72323c31f0e7bca5abaf5e86036828c03814a8f
Matthew Bateson <batesonmatt@yahoo.com>
2024-06-12 01:33:14 -0500

    Added GetTimeScale method to retrieve the underlying time scale type.

--- DST.Core/Trajectory/ITrajectory.cs
+++ DST.Core/Trajectory/ITrajectory.cs
@@ -1,9 +1,11 @@
 ﻿using DST.Core.DateAndTime;
+using DST.Core.TimeScalable;
 
 namespace DST.Core.Trajectory
 {
     public interface ITrajectory
     {
+        TimeScale GetTimeScale();
         bool IsAboveHorizon(IAstronomicalDateTime dateTime);
     }
 }
