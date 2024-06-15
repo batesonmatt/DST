a5d9c716af6939f5f8e5a79cc952280cbc4c1be3
Matthew Bateson <batesonmatt@yahoo.com>
2024-06-15 15:05:11 -0500

    Added new TextValuePair type to encapsulate a displayable text property with a string value intended to be used in the URL route list.

---
+++ DST/Models/BusinessLogic/TextValuePair.cs
@@ -0,0 +1,24 @@
+﻿using DST.Models.Extensions;
+
+namespace DST.Models.BusinessLogic
+{
+    public class TextValuePair
+    {
+        #region Properties
+
+        public string Text { get; }
+        public string Value { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public TextValuePair(string text, string value)
+        {
+            Text = string.IsNullOrWhiteSpace(text) ? string.Empty : text;
+            Value = string.IsNullOrWhiteSpace(value) ? string.Empty : value.ToKebabCase();
+        }
+
+        #endregion
+    }
+}
