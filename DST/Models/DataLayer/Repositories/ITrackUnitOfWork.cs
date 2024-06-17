471baf5c9745cc70e43ad08fa26bcf07b2889ccc
Matthew Bateson <batesonmatt@yahoo.com>
2024-06-16 22:35:55 -0500

    Extracted ITrackUnitOfWork interface from type TrackUnitOfWork.

---
+++ DST/Models/DataLayer/Repositories/ITrackUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using DST.Models.DomainModels;
+
+namespace DST.Models.DataLayer.Repositories
+{
+    public interface ITrackUnitOfWork
+    {
+        Repository<DsoModel> DsoItems { get; }
+
+        ConstellationModel GetConstellation(DsoModel dso);
+        SeasonModel GetSeason(DsoModel dso);
+    }
+}
