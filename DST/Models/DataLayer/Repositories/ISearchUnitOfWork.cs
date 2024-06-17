813a7291834f4586349bbd7d2ed4d9c8e6b0f5d2
Matthew Bateson <batesonmatt@yahoo.com>
2024-06-16 22:35:29 -0500

    Extracted ISearchUnitOfWork interface from type SearchUnitOfWork.

---
+++ DST/Models/DataLayer/Repositories/ISearchUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using DST.Models.DomainModels;
+
+namespace DST.Models.DataLayer.Repositories
+{
+    public interface ISearchUnitOfWork
+    {
+        Repository<DsoModel> DsoItems { get; }
+        Repository<DsoTypeModel> DsoTypes { get; }
+        Repository<CatalogModel> Catalogs { get; }
+        Repository<ConstellationModel> Constellations { get; }
+        Repository<SeasonModel> Seasons { get; }
+    }
+}
