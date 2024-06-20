8c8abaddbb9948dfc2a14e5590a076bf9269bc50
Matthew Bateson <batesonmatt@yahoo.com>
2024-06-19 22:32:21 -0500

    Added TrajectoryName class to encapsulate the displayable trajectory types.

---
+++ DST/Models/BusinessLogic/TrajectoryName.cs
@@ -0,0 +1,31 @@
+﻿using DST.Models.DataLayer.Query;
+using System.Collections.Generic;
+
+namespace DST.Models.BusinessLogic
+{
+    public static class TrajectoryName
+    {
+        #region Properties
+
+        public static string Circumpolar => Resources.DisplayText.TrajectoryCircumpolar;
+        public static string NeverRise => Resources.DisplayText.TrajectoryNeverRise;
+        public static string RiseAndSet => Resources.DisplayText.TrajectoryRiseAndSet;
+
+        #endregion
+
+        #region Methods
+
+        public static IEnumerable<TextValuePair> GetTextValuePairs()
+        {
+            return new TextValuePair[]
+            {
+                new(Filter.All, Filter.All),
+                new(Circumpolar, Circumpolar),
+                new(NeverRise, NeverRise),
+                new(RiseAndSet, RiseAndSet)
+            };
+        }
+
+        #endregion
+    }
+}
