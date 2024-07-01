18a884f53aa70ba097aad78519be330025fe1d3b
Matthew Bateson <batesonmatt@yahoo.com>
2024-07-01 00:41:49 -0500

    Added Filter.Any string.

--- DST/Models/DataLayer/Query/Filter.cs
+++ DST/Models/DataLayer/Query/Filter.cs
@@ -5,6 +5,7 @@
         #region Properties
 
         public static string All => Resources.DisplayText.FilterAll;
+        public static string Any => Resources.DisplayText.FilterAny;
         public static string Off => Resources.DisplayText.FilterOff;
         public static string On => Resources.DisplayText.FilterOn;
         public static string Unknown => Resources.DisplayText.FilterUnknown;
