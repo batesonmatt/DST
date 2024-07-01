c8fb464f6d9c1ad4b4454827f85ca283e1452471
Matthew Bateson <batesonmatt@yahoo.com>
2024-07-01 00:50:29 -0500

    Added optional ISortFilterBuilder interface and SortFilterBuilder implementation to be used in the future.

---
+++ DST/Models/Builders/ISortFilterBuilder.cs
@@ -0,0 +1,9 @@
+﻿using DST.Models.DomainModels;
+
+namespace DST.Models.Builders
+{
+    public interface ISortFilterBuilder : IBuilder
+    {
+        SortFilterModel CurrentSortFilter { get; set; }
+    }
+}
---
+++ DST/Models/Builders/SortFilterBuilder.cs
@@ -0,0 +1,61 @@
+﻿using DST.Models.DomainModels;
+using DST.Models.Extensions;
+using Microsoft.AspNetCore.Http;
+
+namespace DST.Models.Builders
+{
+    public class SortFilterBuilder : ISortFilterBuilder
+    {
+        #region Properties
+
+        public SortFilterModel CurrentSortFilter { get; set; }
+
+        #endregion
+
+        #region Fields
+
+        private const string _sortFilterKey = "sortFilterKey";
+        private readonly ISession _session;
+
+        #endregion
+
+        #region Constructors
+
+        public SortFilterBuilder(IHttpContextAccessor context)
+        {
+            _session = context.HttpContext!.Session;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Load()
+        {
+            // Try to load from session state.
+            // Create new object if no value is stored.
+            CurrentSortFilter = _session.GetObject<SortFilterModel>(_sortFilterKey) ?? new SortFilterModel();
+
+            // Save the current object to session state.
+            Save();
+        }
+
+        public void Save()
+        {
+            switch (CurrentSortFilter)
+            {
+                // If set to null, remove the object from session state.
+                case null:
+                    _session.Remove(_sortFilterKey);
+                    break;
+
+                // Save to session state.
+                default:
+                    _session.SetObject(_sortFilterKey, CurrentSortFilter);
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
