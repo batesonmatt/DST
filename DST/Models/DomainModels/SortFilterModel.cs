e521206f3d4048e9011d34584828f988449197e2
Matthew Bateson <batesonmatt@yahoo.com>
2024-07-01 00:51:29 -0500

    Added SortFilterModel to be used for submitting the search sorting/filtering options in a form.

---
+++ DST/Models/DomainModels/SortFilterModel.cs
@@ -0,0 +1,22 @@
+﻿using DST.Models.DataLayer.Query;
+
+namespace DST.Models.DomainModels
+{
+    public class SortFilterModel
+    {
+        #region Properties
+
+        public string SortField { get; set; } = Sort.Default;
+
+        public string Catalog { get; set; } = Filter.All;
+        public string Type { get; set; } = Filter.All;
+        public string Constellation { get; set; } = Filter.All;
+        public string Season { get; set; } = Filter.All;
+        public string Trajectory { get; set; } = Filter.All;
+        public string Visibility { get; set; } = Filter.Any;
+        
+        public bool IsFilterByHasName { get; set; } = false;
+
+        #endregion
+    }
+}
