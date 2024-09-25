ce7ff945b67556601e50a936d05534bd56803356
Matthew Bateson <batesonmatt@yahoo.com>
2024-09-25 13:02:22 -0500

    Added toggleCollapseIcon JS function.

--- DST/wwwroot/js/misc.js
+++ DST/wwwroot/js/misc.js
@@ -9,3 +9,8 @@
 function focusElement(elementId) {
     setTimeout(() => { document.getElementById(elementId).focus() }, 1000);
 }
+
+function toggleCollapseIcon(elementId) {
+    let e = document.getElementById(elementId);
+    e.classList.toggle("bi-chevron-compact-up");
+}
