a68938e5af51a8cfd84f1430808f90cb91b4b15b
Matthew Bateson <batesonmatt@yahoo.com>
2024-10-03 21:59:16 -0500

    Added SortDirection and PageSize properties to SortFilterModel for binding in the search options modal form. Also added IsSortAscending and IsSortDescending helper properties.

--- DST/Models/DomainModels/SortFilterModel.cs
+++ DST/Models/DomainModels/SortFilterModel.cs
@@ -1,4 +1,6 @@
 ﻿using DST.Models.DataLayer.Query;
+using DST.Models.Extensions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace DST.Models.DomainModels
 {
@@ -7,6 +9,8 @@ namespace DST.Models.DomainModels
         #region Properties
 
         public string SortField { get; set; } = Sort.Default;
+        public string SortDirection { get; set; } = OrderDirection.Default;
+        public int PageSize { get; set; } = Paging.DefaultPageSize;
 
         public string Catalog { get; set; } = Filter.All;
         public string Type { get; set; } = Filter.All;
@@ -16,6 +20,12 @@ namespace DST.Models.DomainModels
         public string Visibility { get; set; } = Filter.Any;
         
         public bool IsFilterByHasName { get; set; } = false;
+
+        [BindNever]
+        public bool IsSortAscending => SortDirection.EqualsSeo(OrderDirection.Ascending);
+
+        [BindNever]
+        public bool IsSortDescending => SortDirection.EqualsSeo(OrderDirection.Descending);
 
         #endregion
     }
