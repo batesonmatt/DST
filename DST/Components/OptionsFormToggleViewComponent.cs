9c015484c0ddf3d95d4abe794d7d74521bba6805
Matthew Bateson <batesonmatt@yahoo.com>
2025-01-10 00:20:04 -0600

    Added a view component for invoking the partial view for the options form toggle button.

---
+++ DST/Components/OptionsFormToggleViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using DST.Models.Extensions;
+using DST.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DST.Components
+{
+    public class OptionsFormToggleViewComponent : ViewComponent
+    {
+        #region Constructors
+
+        public OptionsFormToggleViewComponent() { }
+
+        #endregion
+
+        #region Methods
+
+        public IViewComponentResult Invoke(string formId, bool enabled = true)
+        {
+            if (string.IsNullOrWhiteSpace(formId))
+            {
+                formId = string.Empty;
+            }
+
+            FormToggleViewModel viewModel = new()
+            {
+                FormId = formId,
+                EnabledState = enabled.Disabled()
+            };
+
+            return View("~/Views/Shared/_OptionsFormTogglePartial.cshtml", viewModel);
+        }
+
+        #endregion
+    }
+}
