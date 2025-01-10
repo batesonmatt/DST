17eaa7b6d8ed9d8e093e2a11c3933f3f2f7a4cbd
Matthew Bateson <batesonmatt@yahoo.com>
2025-01-10 00:17:06 -0600

    Added FormToggleViewModel class to encapsulate a form ID string and enabled state, intended to be used to create toggle buttons for forms contained within Bootstrap modal components.

---
+++ DST/Models/ViewModels/FormToggleViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DST.Models.ViewModels
+{
+    public class FormToggleViewModel
+    {
+        #region Properties
+
+        public string FormId { get; set; }
+        public string EnabledState { get; set; }
+
+        #endregion
+    }
+}
