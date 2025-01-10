ca3347877c30a237381d095f9d7b1275b67f02bd
Matthew Bateson <batesonmatt@yahoo.com>
2025-01-10 00:19:49 -0600

    Added a view component for invoking the partial view for the geolocation form toggle button.

---
+++ DST/Components/GeolocationFormToggleViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using DST.Models.Extensions;
+using DST.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DST.Components
+{
+    public class GeolocationFormToggleViewComponent : ViewComponent
+    {
+        #region Constructors
+
+        public GeolocationFormToggleViewComponent() { }
+
+        #endregion
+
+        #region Methods
+
+        public IViewComponentResult Invoke(string formId, bool enabled = true)
+        {
+            if (string.IsNullOrWhiteSpace(formId))
+            {
+                formId = string.Empty;
+            }
+
+            FormToggleViewModel viewModel = new()
+            {
+                FormId = formId,
+                EnabledState = enabled.Disabled()
+            };
+
+            return View("~/Views/Shared/_GeolocationFormTogglePartial.cshtml", viewModel);
+        }
+
+        #endregion
+    }
+}
