843416323cb31eef23835f49822d8b19a44f2dba
Matthew Bateson <batesonmatt@yahoo.com>
2025-01-12 13:19:00 -0600

    Added AlertMessage class to encapsulate a message and the severity of the alert.

---
+++ DST/Models/BusinessLogic/AlertMessage.cs
@@ -0,0 +1,22 @@
+﻿namespace DST.Models.BusinessLogic
+{
+    public class AlertMessage
+    {
+        #region Properties
+
+        public string Message { get; set; }
+        public AlertType Type { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public AlertMessage(string message, AlertType type)
+        {
+            Message = string.IsNullOrWhiteSpace(message) ? string.Empty : message.Trim();
+            Type = type;
+        }
+
+        #endregion
+    }
+}
