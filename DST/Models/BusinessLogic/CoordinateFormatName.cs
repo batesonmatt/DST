7905a88af7cba6fe98741416dff1d7d72178bc3b
Matthew Bateson <batesonmatt@yahoo.com>
2025-01-16 01:10:34 -0600

    Added static CoordinateFormatName class with static string properties for the coordinate format options.

---
+++ DST/Models/BusinessLogic/CoordinateFormatName.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace DST.Models.BusinessLogic
+{
+    public static class CoordinateFormatName
+    {
+        #region Properties
+
+        public static string Component => Resources.DisplayText.CoordinateFormatComponent;
+        public static string Decimal => Resources.DisplayText.CoordinateFormatDecimal;
+        public static string Compact => Resources.DisplayText.CoordinateFormatCompact;
+
+        public static string Default => Component;
+
+        #endregion
+
+        #region Methods
+
+        public static IEnumerable<TextValuePair> GetTextValuePairs()
+        {
+            return new TextValuePair[]
+            {
+                new(Component, Component),
+                new(Decimal, Decimal),
+                new(Compact, Compact)
+            };
+        }
+
+        #endregion
+    }
+}
