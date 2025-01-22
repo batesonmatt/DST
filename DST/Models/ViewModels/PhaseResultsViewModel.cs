1872e5b13fca17971411f3624551c1b94ba827a5
Matthew Bateson <batesonmatt@yahoo.com>
2025-01-22 15:07:01 -0600

    Added TrackPhaseViewModel, deriving from TrackResultsViewModel, to encapsulate the specific displayable information for the DSO phase tracking.

---
+++ DST/Models/ViewModels/PhaseResultsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DST.Models.ViewModels
+{
+    public class PhaseResultsViewModel : TrackResultsViewModel
+    {
+        #region Properties
+
+        public string Phase { get; set; }
+        public string Cycles { get; set; }
+
+        #endregion
+    }
+}
