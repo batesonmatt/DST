57f8c96fff4a4b3c40529171660e7ddaeb70afab
Matthew Bateson <batesonmatt@yahoo.com>
2025-01-22 15:04:40 -0600

    Added new TrackResultsViewModel class for encapsulating the base displayable information for the DSO tracking results.

---
+++ DST/Models/ViewModels/TrackResultsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DST.Models.BusinessLogic;
+using System.Collections.Generic;
+
+namespace DST.Models.ViewModels
+{
+    public class TrackResultsViewModel
+    {
+        #region Properties
+
+        public string TimeKeeper { get; set; }
+        public string TimeScale { get; set; }
+        public string Start { get; set; }
+        public IEnumerable<TrackResult> Results { get; set; }
+
+        #endregion
+    }
+}
