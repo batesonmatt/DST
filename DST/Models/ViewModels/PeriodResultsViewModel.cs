a73cc6b0a9c5f5fcb7bfc137afb04b5470912173
Matthew Bateson <batesonmatt@yahoo.com>
2025-01-22 15:06:34 -0600

    Added PeriodResultsViewModel, deriving from TrackResultsViewModel, to encapsulate the specific displayable information for the DSO period tracking.

---
+++ DST/Models/ViewModels/PeriodResultsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DST.Models.ViewModels
+{
+    public class PeriodResultsViewModel : TrackResultsViewModel
+    {
+        #region Properties
+
+        public string TimeUnit { get; set; }
+        public string Period { get; set; }
+        public string Interval { get; set; }
+        public string Fixed { get; set; }
+        public string Aggregated { get; set; }
+
+        #endregion
+    }
+}
