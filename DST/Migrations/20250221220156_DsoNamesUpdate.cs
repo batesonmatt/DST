dbac7b9efc2939bcdda4fe6d835d0d39aa44a290
Matthew Bateson <batesonmatt@yahoo.com>
2025-02-21 16:07:29 -0600

    Added new migration, DsoNamesUpdate.

---
+++ DST/Migrations/20250221220156_DsoNamesUpdate.cs
@@ -0,0 +1,466 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DST.Migrations
+{
+    /// <inheritdoc />
+    public partial class DsoNamesUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 1 },
+                column: "Common",
+                value: "Polarissima Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 8 },
+                column: "Common",
+                value: "Ghost's Goblet");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 70 },
+                column: "Common",
+                value: "Sculptor Pinwheel Galaxy");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 71 },
+                column: "Common",
+                value: "Termite Hole Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 72 },
+                column: "Common",
+                value: "String of Pearls Galaxy");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 76 },
+                column: "Common",
+                value: "Baby Scorpion Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 101 },
+                column: "Common",
+                value: "Pavo Galaxy");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 4 },
+                column: "Common",
+                value: "Spider Globular Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 15 },
+                column: "Common",
+                value: "Great Pegasus Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 18 },
+                column: "Common",
+                value: "Black Swan Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 21 },
+                column: "Common",
+                value: "Webb's Cross");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 30 },
+                column: "Common",
+                value: "Jellyfish Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 33 },
+                column: "Common",
+                value: "Triangulum Galaxy");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 34 },
+                column: "Common",
+                value: "Spiral Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 35 },
+                column: "Common",
+                value: "Shoe-Buckle Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 36 },
+                column: "Common",
+                value: "Pinwheel Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 37 },
+                column: "Common",
+                value: "Salt and Pepper Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 39 },
+                column: "Common",
+                value: "Pyramid Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 41 },
+                column: "Common",
+                value: "Little Beehive Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 50 },
+                column: "Common",
+                value: "Heart-shaped Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 52 },
+                column: "Common",
+                value: "Scorpion Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 55 },
+                column: "Common",
+                value: "Specter Cluster,Summer Rose Star");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 61 },
+                column: "Common",
+                value: "Swelling Spiral Galaxy");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 62 },
+                column: "Common",
+                value: "Flickering Globular Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 67 },
+                column: "Common",
+                value: "King Cobra Cluster,Golden Eye Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 71 },
+                column: "Common",
+                value: "Angelfish Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 93 },
+                column: "Common",
+                value: "Critter Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 99 },
+                column: "Common",
+                value: "St. Catherine's Wheel,Coma Pinwheel Galaxy,Virgo Cluster Pinwheel");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 100 },
+                column: "Common",
+                value: "Mirror Galaxy");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 107 },
+                column: "Common",
+                value: "Crucifix Cluster");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 108 },
+                column: "Common",
+                value: "Surfboard Galaxy");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 109 },
+                column: "Common",
+                value: "Vacuum Cleaner Galaxy");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 1 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 8 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 70 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 71 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 72 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 76 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Caldwell", 101 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 4 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 15 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 18 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 21 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 30 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 33 },
+                column: "Common",
+                value: "Triangulum Galaxy,Pinwheel Galaxy");
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 34 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 35 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 36 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 37 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 39 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 41 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 50 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 52 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 55 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 61 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 62 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 67 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 71 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 93 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 99 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 100 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 107 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 108 },
+                column: "Common",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "DsoItems",
+                keyColumns: new[] { "CatalogName", "Id" },
+                keyValues: new object[] { "Messier", 109 },
+                column: "Common",
+                value: null);
+        }
+    }
+}
