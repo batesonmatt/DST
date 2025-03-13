eac9c3e385f36f6e824efc00cc90ed5f1742c05a
Matthew Bateson <batesonmatt@yahoo.com>
2025-03-12 22:36:58 -0500

    Injected a IHomeUnitOfWork object into the Index action method, and defined a new HomeViewModel instance for the view.

--- DST/Controllers/HomeController.cs
+++ DST/Controllers/HomeController.cs
@@ -1,17 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DST.Models.DataLayer.Repositories;
+using DST.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DST.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        #region "Methods"
+
+        public IActionResult Index([FromServices] IHomeUnitOfWork data)
         {
-            return View(); // "Index"
+            HomeViewModel model = new()
+            {
+                DsoItems = data.GetRandomDsoItems(5)
+            };
+
+            return View(model); // "Index"
         }
 
         public IActionResult About()
         {
             return View(); // "About"
         }
+
+        #endregion
     }
 }
