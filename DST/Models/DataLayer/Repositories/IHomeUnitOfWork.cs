6395784edaf3120c737a7ced790c54ea1e5a7ce6
Matthew Bateson <batesonmatt@yahoo.com>
2025-03-12 22:31:52 -0500

    Added IHomeUnitOfWork interface for accessing data in the HomeController.

---
+++ DST/Models/DataLayer/Repositories/IHomeUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using DST.Models.DomainModels;
+using System.Collections.Generic;
+
+namespace DST.Models.DataLayer.Repositories
+{
+    public interface IHomeUnitOfWork
+    {
+        Repository<DsoModel> DsoItems { get; }
+
+        IEnumerable<DsoModel> GetRandomDsoItems(int count);
+    }
+}
