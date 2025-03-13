000580052db03b7cc9cb3aea14cd434e9d6b730b
Matthew Bateson <batesonmatt@yahoo.com>
2025-03-12 22:34:05 -0500

    Added HomeViewModel for use in the HomeController.

---
+++ DST/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DST.Models.DomainModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DST.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        #region "Properties"
+
+        public IEnumerable<DsoModel> DsoItems { get; set; }
+        public int DsoCount => DsoItems?.Count() ?? 0;
+
+        #endregion
+    }
+}
